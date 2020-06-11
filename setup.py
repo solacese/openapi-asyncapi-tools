6259aff716276c1148fce1aac9972a5c1d2c2cb3
flyisland <flyisland@gmail.com>
2020-06-11 13:26:20 +0800

    refactor: rename to cmd.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "importOpenAPI=sep_tools.importOAS:cmdImportOpenAPI",
+            "importOpenAPI=sep_tools.cmd:cmdImportOpenAPI",
         ]
     },
 )
