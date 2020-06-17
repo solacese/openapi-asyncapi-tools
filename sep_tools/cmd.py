b9dafc136f11632aebec27310bec4aa9452845c6
flyisland <flyisland@gmail.com>
2020-06-18 00:20:00 +0800

    wip: add generateOpenAPI command

--- sep_tools/cmd.py
+++ sep_tools/cmd.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 def cli():
     pass
 
+# -------------------------- importOpenAPI --------------------------
 @cli.command(name="importOpenAPI")
 @click.argument('open_api_spec_file', type=click.Path(exists=True))
 @click.option('--domain', default="TestDomain", show_default=True, 
@@ -27,18 +28,34 @@ def cmdImportOpenAPI(open_api_spec_file, domain, application, token):
     ep = EventPortal(token)
     ep.importOpenAPISpec(open_api_spec_file, domain, application)
 
+# -------------------------- generateAsyncAPI --------------------------
 @cli.command(name="generateAsyncAPI")
 @click.argument('application')
 @click.option('--token', envvar='EVENT_PORTAL_TOKEN', required=True,
     help="The API token of Solace's Cloud REST API, could be set with env variable [EVENT_PORTAL_TOKEN]")
 def generateAsyncAPI(application, token):
-    """Generate a AsyncAPI spec for the specified Application"""
+    """Generate an AsyncAPI spec for the specified Application"""
 
     logging.info("Generate AsyncAPI spec for the Application '{}'".format(
          application
     ))
     ep = EventPortal(token)
     ep.generateAsyncApi(application)
+
+# -------------------------- generateOpenAPI --------------------------
+@cli.command(name="generateOpenAPI")
+@click.argument('domain-name')
+@click.option('--token', envvar='EVENT_PORTAL_TOKEN', required=True,
+    help="The API token of Solace's Cloud REST API, could be set with env variable [EVENT_PORTAL_TOKEN]")
+def generateOpenApi(domain_name, token):
+    """Generate a OpenAPI spec for the specified Domain that represents all the external events that the domain receives"""
+
+    logging.info("Generate OpenAPI spec for the Application Domain '{}'".format(
+         domain_name
+    ))
+    ep = EventPortal(token)
+    ep.generateOpenApi(domain_name)
+
 
 if __name__ == '__main__':
     cli()
